b596d16e6817cb39ad065ebd5c4857009d2c45a5
Edson Rodrigues <edson091990@gmail.com>
2023-10-23 01:38:04 -0300

    Popup window

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <button id="popupButton" class"popup" onclick="myFunction()">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+
+                    <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        </button>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+
+function myFunction(){
+    let popup = document.getElementById("popupButton")
+    let params = 
+        `
+        scrollbars=no,resizable=no,status=no,
+        location=no,toolbar=no,menubar=no,
+        width=300,height=500,left=-1000,top=-1000
+        `;
+    let newWin = open('about:blank', 'test', params)
+    newWin.focus()
+    let html = `<div>Teste</div>`
+    newWin.document.body.insertAdjacentHTML('afterbegin', html)
+}
